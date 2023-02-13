559fc9aa9a9677375d26d5d5bc00ddb9afc65f41
yashahmad <yash.ahd786@gmail.com>
2023-02-13 23:13:14 +0530

    added starter page

---
+++ src/modules/home/components/Home.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <div className="flex flex-col items-center py-24">
+            <h1 className="text-5xl font-extrabold leading-tight sm:text-extra">
+                RT WhiteBoard
+            </h1>
+        </div>
+    )
+}
+
+export default Home;

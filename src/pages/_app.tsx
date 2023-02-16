65183050254a184bb2963ffb7a1af43daac9b408
yashahmad <yash.ahd786@gmail.com>
2023-02-16 13:34:08 +0530

    added state management with recoil

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,30 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+
+import { MotionConfig } from 'framer-motion';
+import Head from 'next/head';
+import { ToastContainer } from 'react-toastify';
+import { RecoilRoot } from 'recoil';
+
+// import { DEFAULT_EASE } from '@/common/constants/easings';
+// import { ModalManager } from '@/modules/modal';
+
+import 'react-toastify/dist/ReactToastify.min.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>RealTime Whiteboard</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <RecoilRoot>
+        <ToastContainer/>
+        {/* <MotionConfig transition={{ ease: DEFAULT_EASE }}> */}
+          {/* <ModalManager /> */}
+          <Component {...pageProps} />
+        {/* </MotionConfig> */}
+      </RecoilRoot>
+    </>
+  )
 }

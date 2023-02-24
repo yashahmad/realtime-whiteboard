e8e743d57905e737f32217e7f0eb4ced458f2c43
yashahmad <yash.ahd786@gmail.com>
2023-02-24 21:26:49 +0530

    added recoil state and hook for options

---
+++ src/common/recoil/options/options.atom.ts
@@ -0,0 +1,15 @@
+import { atom } from 'recoil';
+
+import { CtxOptions } from '@/common/types/global';
+
+export const optionsAtom = atom<CtxOptions>({
+    key: 'options',
+    default: {
+        lineColor: { r: 0, g: 0, b: 0, a: 1 },
+        fillColor: { r: 0, g: 0, b: 0, a: 0 },
+        lineWidth: 5,
+        mode: 'draw',
+        shape: 'line',
+        selection: null,
+    },
+});

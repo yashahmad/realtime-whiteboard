d2d021e938b01afdb2fe49c37fef1a93de1dbbcc
yashahmad <yash.ahd786@gmail.com>
2023-02-24 21:33:36 +0530

    added modal component and hook

--- src/modules/modal/components/ModalManager.tsx
+++ src/modules/modal/components/ModalManager.tsx
@@ -6,56 +6,56 @@ import { useRecoilState } from 'recoil';
 import Portal from '@/common/components/portal/components/Portal';
 
 import {
-    bgAnimation,
-    modalAnimation,
+  bgAnimation,
+  modalAnimation,
 } from '../animations/ModalManager.animations';
 import { modalAtom } from '../recoil/modal.atom';
 
 const ModalManager = () => {
-    const [{ opened, modal }, setModal] = useRecoilState(modalAtom);
+  const [{ opened, modal }, setModal] = useRecoilState(modalAtom);
 
-    const [portalNode, setPortalNode] = useState<HTMLElement>();
+  const [portalNode, setPortalNode] = useState<HTMLElement>();
 
-    useEffect(() => {
-        if (!portalNode) {
-            const node = document.getElementById('portal');
-            if (node) setPortalNode(node);
-            return;
-        }
+  useEffect(() => {
+    if (!portalNode) {
+      const node = document.getElementById('portal');
+      if (node) setPortalNode(node);
+      return;
+    }
 
-        if (opened) {
-            portalNode.style.pointerEvents = 'all';
-        } else {
-            portalNode.style.pointerEvents = 'none';
-        }
-    }, [opened, portalNode]);
+    if (opened) {
+      portalNode.style.pointerEvents = 'all';
+    } else {
+      portalNode.style.pointerEvents = 'none';
+    }
+  }, [opened, portalNode]);
 
-    return (
-        <Portal>
+  return (
+    <Portal>
+      <motion.div
+        className="absolute z-40 flex min-h-full w-full items-center justify-center bg-black/80"
+        onClick={() => setModal({ modal: <></>, opened: false })}
+        variants={bgAnimation}
+        initial="closed"
+        animate={opened ? 'opened' : 'closed'}
+      >
+        <AnimatePresence>
+          {opened && (
             <motion.div
-                className="absolute z-40 flex min-h-full w-full items-center justify-center bg-black/80"
-                onClick={() => setModal({ modal: <></>, opened: false })}
-                variants={bgAnimation}
-                initial="closed"
-                animate={opened ? 'opened' : 'closed'}
+              variants={modalAnimation}
+              initial="closed"
+              animate="opened"
+              exit="exited"
+              onClick={(e) => e.stopPropagation()}
+              className="p-6"
             >
-                <AnimatePresence>
-                    {opened && (
-                        <motion.div
-                            variants={modalAnimation}
-                            initial="closed"
-                            animate="opened"
-                            exit="exited"
-                            onClick={(e) => e.stopPropagation()}
-                            className="p-6"
-                        >
-                            {modal}
-                        </motion.div>
-                    )}
-                </AnimatePresence>
+              {modal}
             </motion.div>
-        </Portal>
-    );
+          )}
+        </AnimatePresence>
+      </motion.div>
+    </Portal>
+  );
 };
 
 export default ModalManager;
--- src/modules/modal/recoil/modal.hooks.tsx
+++ src/modules/modal/recoil/modal.hooks.tsx
@@ -1,11 +1,11 @@
-import { useSetRecoilState } from "recoil";
+import { useSetRecoilState } from 'recoil';
 
 import { modalAtom } from './modal.atom';
 
 const useModal = () => {
     const setModal = useSetRecoilState(modalAtom);
 
-    const openModal = (modal: JSX.Element | JSX.Element[]) => 
+    const openModal = (modal: JSX.Element | JSX.Element[]) =>
         setModal({ modal, opened: true });
 
     const closeModal = () => setModal({ modal: <></>, opened: false });

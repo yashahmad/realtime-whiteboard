ce88ae5c1947eef8b6088c7c34941547a9dc94c3
yashahmad <yash.ahd786@gmail.com>
2023-02-24 21:27:18 +0530

    added recoil state and hook for savedmoves

---
+++ src/common/recoil/savedMoves/savedMoves.atom.ts
@@ -0,0 +1,8 @@
+import { atom } from 'recoil';
+
+import { Move } from '@/common/types/global';
+
+export const savedMovesAtom = atom<Move[]>({
+    key: 'saved_moves',
+    default: [],
+});
---
+++ src/common/recoil/savedMoves/savedMoves.hooks.ts
@@ -0,0 +1,39 @@
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+
+import { Move } from '@/common/types/global';
+
+import { savedMovesAtom } from './savedMoves.atom';
+
+export const useSetSavedMoves = () => {
+    const setSavedMoves = useSetRecoilState(savedMovesAtom);
+
+    const addSavedMove = (move: Move) => {
+        if (move.options.mode === 'select') return;
+
+        setSavedMoves((prevMoves) => [move, ...prevMoves]);
+    };
+
+    const removeSavedMove = () => {
+        let move: Move | undefined;
+
+        setSavedMoves((prevMoves) => {
+            move = prevMoves.at(0);
+
+            return prevMoves.slice(1);
+        });
+
+        return move;
+    };
+
+    const clearSavedMoves = () => {
+        setSavedMoves([]);
+    };
+
+    return { addSavedMove, removeSavedMove, clearSavedMoves };
+};
+
+export const useSavedMoves = () => {
+    const savedMoves = useRecoilValue(savedMovesAtom);
+
+    return savedMoves;
+};

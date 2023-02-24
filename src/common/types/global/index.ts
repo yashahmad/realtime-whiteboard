c61fc12387a7abe463a25a1e846cb8c5ef607db4
yashahmad <yash.ahd786@gmail.com>
2023-02-24 21:29:16 +0530

    added global types

--- src/common/types/global/index.ts
+++ src/common/types/global/index.ts
@@ -1,4 +1,4 @@
-import { RgbaColor } from "react-colorful";
+import { RgbaColor } from 'react-colorful';
 
 export type Shape = 'line' | 'circle' | 'rect' | 'image';
 export type CtxMode = 'eraser' | 'draw' | 'select';
@@ -15,7 +15,7 @@ export interface CtxOptions {
         width: number;
         height: number;
     } | null;
-};
+}
 
 export interface Move {
     circle: {
@@ -35,7 +35,7 @@ export interface Move {
     options: CtxOptions;
     timestamp: number;
     id: string;
-};
+}
 
 export type Room = {
     usersMoves: Map<string, Move[]>;
@@ -46,7 +46,7 @@ export type Room = {
 export interface User {
     name: string;
     color: string;
-};
+}
 
 export interface ClientRoom {
     id: string;
@@ -54,7 +54,7 @@ export interface ClientRoom {
     movesWithoutUser: Move[];
     myMoves: Move[];
     users: Map<string, User>;
-};
+}
 
 export interface MessageType {
     userId: string;
@@ -64,8 +64,28 @@ export interface MessageType {
     id: number;
 }
 
-// export interface ServerToClientEvents {
-// }
+export interface ServerToClientEvents {
+    room_exists: (exists: boolean) => void;
+    joined: (roomId: string, failed?: boolean) => void;
+    room: (room: Room, usersMovesToParse: string, usersToParse: string) => void;
+    created: (roomId: string) => void;
+    your_move: (move: Move) => void;
+    user_draw: (move: Move, userId: string) => void;
+    user_undo(userId: string): void;
+    mouse_moved: (x: number, y: number, userId: string) => void;
+    new_user: (userId: string, username: string) => void;
+    user_disconnected: (userId: string) => void;
+    new_msg: (userId: string, msg: string) => void;
+}
 
-// export interface ClientToServerEvents {
-// }
+export interface ClientToServerEvents {
+    check_room: (roomId: string) => void;
+    draw: (move: Move) => void;
+    mouse_move: (x: number, y: number) => void;
+    undo: () => void;
+    create_room: (username: string) => void;
+    join_room: (room: string, username: string) => void;
+    joined_room: () => void;
+    leave_room: () => void;
+    send_msg: (msg: string) => void;
+}

b2c525b6bb4f2fa6199b022cf5bd0116262e027b
yashahmad <yash.ahd786@gmail.com>
2023-02-24 21:35:19 +0530

    added room component

---
+++ src/modules/room/components/Room.tsx
@@ -0,0 +1,27 @@
+import { useRoom } from '@/common/recoil/room';
+
+import RoomContextProvider from '../context/Room.context';
+import Board from '../modules/board';
+import Chat from '../modules/chat';
+import ToolBar from '../modules/toolbar';
+import NameInput from './NameInput';
+import UserList from './UserList';
+
+const Room = () => {
+    const room = useRoom();
+
+    if (!room.id) return <NameInput />;
+
+    return (
+        <RoomContextProvider>
+            <div className="relative h-full w-full overflow-hidden">
+                <UserList />
+                <ToolBar />
+                <Board />
+                <Chat />
+            </div>
+        </RoomContextProvider>
+    );
+};
+
+export default Room;

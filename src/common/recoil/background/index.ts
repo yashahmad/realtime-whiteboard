c96b0b14e8d01120e86695c3835682ea882cd89d
yashahmad <yash.ahd786@gmail.com>
2023-02-24 21:26:34 +0530

    added recoil state and hook for background

---
+++ src/common/recoil/background/index.ts
@@ -0,0 +1,5 @@
+import { backgroundAtom } from './background.atom';
+import { useBackground, useSetBackground } from './background.hooks';
+
+export default backgroundAtom;
+export { useBackground, useSetBackground };
